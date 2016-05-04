49b2cdb8af071fec25a4a1cdd3aea93b46449e69
hdvillarreal <daniel.villarreal12@gmail.com>
2016-05-04 10:51:30 -0600

    Added support for initial sequence off, error detection

--- Serial Data Decoder/src/com/danielvillarreal/TivaSerialDataReader.java
+++ Serial Data Decoder/src/com/danielvillarreal/TivaSerialDataReader.java
@@ -5,6 +5,7 @@ import gnu.io.CommPortIdentifier;
 import gnu.io.SerialPort;
 
 import java.io.BufferedInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
@@ -27,18 +28,28 @@ public class TivaSerialDataReader{
 		//RefineryUtilities.centerFrameOnScreen(demo);
 		//demo.setVisible(true);
 
-		try{
+		try {
 			writer = new PrintWriter("errorDetectionOutput.txt");
-			Runtime.getRuntime().addShutdownHook(new Thread(new ShutdownSaver(writer)));
-			this.connect(port1);
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		Runtime.getRuntime().addShutdownHook(new Thread(new ShutdownSaver(writer)));
+		attempConnection(port1);
+	
+	}
+	
+	void attempConnection(String port){
+		try {
+			this.connect(port);
 			System.out.println("Connected to port!");
-			
-			
-			
-		}
-		catch ( Exception e ){
+		} catch (Exception e) {
 			e.printStackTrace();
+			System.out.println("Retrying...");
+			attempConnection(port);
 		}
+		
+		
 	}
 
 	void connect ( String portName ) throws Exception{
@@ -47,7 +58,7 @@ public class TivaSerialDataReader{
 			System.out.println("Error: Port is currently in use");
 		}
 		else{
-			CommPort commPort = portIdentifier.open(this.getClass().getName(),2000);
+			CommPort commPort = portIdentifier.open(this.getClass().getName(),5000);
 
 
 			if (commPort instanceof SerialPort){
@@ -69,25 +80,25 @@ public class TivaSerialDataReader{
 			}
 		}     
 	}
-	
+
 	public class ShutdownSaver implements Runnable{
 
 		PrintWriter wr;
-		
+
 		public ShutdownSaver(PrintWriter wr) {
 			// TODO Auto-generated constructor stub
 			this.wr = wr;
 		}
-		
+
 		@Override
 		public void run() {
 			// TODO Auto-generated method stub
 			wr.close();
 			wr.flush();
 			System.out.println("closed file");
-			
+
 		}
-		
+
 	}
 
 	public class DataSetAdder implements Runnable{
@@ -135,188 +146,213 @@ public class TivaSerialDataReader{
 			int currentId = -1;
 			int countSinceError = 0;
 			int errorCount = 0;
-			
+			byte temp[];
+
 
 			try{
 
-					while(in.available() > 2){
-						byte temp[] = new byte[2];
-						bis.read(temp);
-						
-						String newSample = Hex.encodeHexString(temp);
-						
-						if(currentId == -1){
-							currentId = extractId(2, newSample);
-						}
-						//System.out.println("New sample: " + newSample);
-						
-						if(extractId(2,newSample) != currentId){
-							
-							
-							bis.mark(3);
-							
-							//Error id mismatch
-							byte discard[] = new byte[2];
-							bis.read(discard);
-							
-							String checkSample = Hex.encodeHexString(discard);
-							if(extractId(2,checkSample) != (currentId+1)){
-								bis.reset();
-								//currentId--;
-							}
-							
-							//System.out.println("Discard: " + Hex.encodeHexString(discard));
-							String debug = ("error-" + currentId + " cse: " + countSinceError + " sample: " + newSample);
-							writer.println(debug);
-							//System.out.println(debug);
-							//in.skip(1);
-							//System.out.println("Error detected!");
-							//System.out.println("Err-Sample,currentId: " + newSample + " ," + currentId );
-							currentId += 2;
-							countSinceError = 0;
-							errorCount++;
-						}else{
-							//System.out.println("OK Smp: " + newSample);
-							writer.println(newSample);
-							currentId++;
-							countSinceError++;
-						}
-						
-						if(currentId > 15){
-							currentId = 0;
-						}
-						
-						//System.out.println(Hex.encodeHexString(temp));
-					}
-
-				}catch ( IOException e ){
-					e.printStackTrace();
+				while(!(bis.available() >= 2)){
+					//Wait until there is enough data
 				}
 				
-			}
+				boolean initialError = true;
 
-			private void checkBlockTransferReady(ArrayList<Sample> data,int blockSize) {
-				if(data.size() >= blockSize){
-					startBlockTranfer(data,blockSize);
+				while(initialError){
+					bis.mark(5);
+					String firstSample = Hex.encodeHexString(readFromStream(this.bis, 2));
+					int temporaryId = extractId(2, firstSample);
+
+					if(detectError(Hex.encodeHexString(readFromStream(this.bis, 2)), temporaryId+1)){
+						//Error, sequence off
+						bis.reset();
+						//Correct sequence by extracting two words
+						readFromStream(bis, 1);
+						writer.println("Sequnece off. Adjusted!");
+					}else{
+						initialError = false;
+						bis.reset();
+					}
 				}
 
-			}
 
-			private void startBlockTranfer(ArrayList<Sample> data,int blockSize){
-				//new Thread(new DataSetAdder(data, demo)).start();
-				(new DataSetAdder(data,demo)).run();
-			}
-		}
+				while(in.available() > 2){
 
-		private void addSetToChart(DynamicDataDemo plot, ArrayList<Sample> data){
+					temp = new byte[2];
+					bis.read(temp);
+					String newSample = Hex.encodeHexString(temp);
 
-			for(Sample s : data){
-				Millisecond now = new Millisecond();
-				plot.addNewSample(s, now, true);
-			}
-		}
+					if(currentId == -1){
+						currentId = extractId(2, newSample);
+					}
+					//System.out.println("New sample: " + newSample);
 
-		private Sample readNextSample(InputStream in, int numberOfBytes, int idPosition){
-			byte data[] = readFromStream(in,numberOfBytes);
-			String resultString = Hex.encodeHexString(data);
-			return (new Sample(extractData(idPosition,resultString),extractId(idPosition,resultString)));
-		}
+					if(extractId(2,newSample) != currentId){
 
-		private Sample rawStringToSample(String rawDataSample, int idPosition){
-			return (new Sample(extractData(idPosition,rawDataSample),extractId(idPosition,rawDataSample)));
-		}
 
-		private byte[] readFromStream(InputStream in, int numberOfBytes){
-			byte result[] = new byte[numberOfBytes];
-			try {
-				if(in.available() >= numberOfBytes){
-					in.read(result);
+						bis.mark(3);
+
+						//Error id mismatch
+						byte discard[] = new byte[2];
+						bis.read(discard);
+
+						String checkSample = Hex.encodeHexString(discard);
+						if(extractId(2,checkSample) != (currentId+1)){
+							bis.reset();
+							currentId--;
+						}
+
+						//System.out.println("Discard: " + Hex.encodeHexString(discard));
+						String debug = ("error-" + currentId + " cse: " + countSinceError + " sample: " + newSample);
+						writer.println(debug);
+						//System.out.println(debug);
+						//in.skip(1);
+						//System.out.println("Error detected!");
+						//System.out.println("Err-Sample,currentId: " + newSample + " ," + currentId );
+						currentId += 2;
+						countSinceError = 0;
+						errorCount++;
+					}else{
+						//System.out.println("OK Smp: " + newSample);
+						writer.println(newSample);
+						currentId++;
+						countSinceError++;
+					}
+
+					if(currentId > 15){
+						currentId = 0;
+					}
+
+					//System.out.println(Hex.encodeHexString(temp));
 				}
-			} catch (IOException e) {
+
+			}catch ( IOException e ){
 				e.printStackTrace();
 			}
-			return result;
+
 		}
 
-		private  int extractId(int idPosition, String data){
-			return Integer.parseInt(extractIdHex(idPosition,data),16);
-		}
-		private String extractIdHex(int idPosition, String data){
-			return data.charAt(idPosition) + "";
+		private boolean detectError(String sample, int currentId){
+			return extractId(2,sample) != currentId;
 		}
 
-		private String extractDataHex(int idPosition, String data){
-			//System.out.println("DBG: extractDataHex" + idPosition);
-			String orderedData = new String();
-			if(idPosition == 2){
-				orderedData = ("" + data.charAt(3) + data.charAt(0) + data.charAt(1));
-			}else if(idPosition == 0){
-				orderedData = ("" + data.charAt(1) + data.charAt(2) + data.charAt(3));
+		private void checkBlockTransferReady(ArrayList<Sample> data,int blockSize) {
+			if(data.size() >= blockSize){
+				startBlockTranfer(data,blockSize);
 			}
 
-			return orderedData;
 		}
 
-		private int extractData(int idPosition, String data){
-
-			return Integer.parseInt(extractDataHex(idPosition,data),16);
-		}
-
-		private static int findIdPosition(String sampleOne, String sampleTwo){
-			byte temp[] = new byte[2];
-
-			char samplePosThree[] = new char[2];
-			char samplePosOne[] = new char[2];
-
-			samplePosThree[0] = sampleOne.charAt(0);
-			samplePosThree[1] = sampleTwo.charAt(0);
-
-			samplePosOne[0] = sampleOne.charAt(2);
-			samplePosOne[1] = sampleTwo.charAt(2);
-
-			//Check whether position three contains the id
-
-			boolean decision[] = new boolean[2];
-			int posThreeValue[] = new int[2];
-			int posOneValue[] = new int[2];
-
-			for(int i = 0; i < 2; i++){
-				posThreeValue[i] = Integer.parseInt(samplePosThree[i] + "",16);
-				posOneValue[i] = Integer.parseInt(samplePosOne[i] + "",16);
-
-			}
-			decision[0] = checkSequence(posThreeValue[0],posThreeValue[1]);
-			decision[1] = checkSequence(posOneValue[0],posOneValue[1]);
-
-			if(!(decision[0]|decision[1])){
-				System.out.println("DBG: SampleOne: " + sampleOne + " SampleTwo: " + sampleTwo);
-			}
-
-			if(decision[0]){
-				return 0;
-			}else if(decision[1]){
-				return 2;
-			}else return -1;
-
-		}
-
-		private static boolean checkSequence(int a, int b){
-
-			if(a == b){
-				return false;
-			}
-
-			if(a > b){
-				return true;
-			}else if(b > a){
-				return true;
-			}else return false;
-
-		}
-
-
-
-		public static void main ( String[] args){
-			new TivaSerialDataReader();
+		private void startBlockTranfer(ArrayList<Sample> data,int blockSize){
+			//new Thread(new DataSetAdder(data, demo)).start();
+			(new DataSetAdder(data,demo)).run();
 		}
 	}
+
+	private void addSetToChart(DynamicDataDemo plot, ArrayList<Sample> data){
+
+		for(Sample s : data){
+			Millisecond now = new Millisecond();
+			plot.addNewSample(s, now, true);
+		}
+	}
+
+
+
+	private Sample rawStringToSample(String rawDataSample, int idPosition){
+		return (new Sample(extractData(idPosition,rawDataSample),extractId(idPosition,rawDataSample)));
+	}
+
+	private byte[] readFromStream(BufferedInputStream bis, int numberOfBytes){
+		byte result[] = new byte[numberOfBytes];
+		try {
+			if(bis.available() >= numberOfBytes){
+				bis.read(result);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	private  int extractId(int idPosition, String data){
+		return Integer.parseInt(extractIdHex(idPosition,data),16);
+	}
+	private String extractIdHex(int idPosition, String data){
+		return data.charAt(idPosition) + "";
+	}
+
+	private String extractDataHex(int idPosition, String data){
+		//System.out.println("DBG: extractDataHex" + idPosition);
+		String orderedData = new String();
+		if(idPosition == 2){
+			orderedData = ("" + data.charAt(3) + data.charAt(0) + data.charAt(1));
+		}else if(idPosition == 0){
+			orderedData = ("" + data.charAt(1) + data.charAt(2) + data.charAt(3));
+		}
+
+		return orderedData;
+	}
+
+	private int extractData(int idPosition, String data){
+
+		return Integer.parseInt(extractDataHex(idPosition,data),16);
+	}
+
+	private static int findIdPosition(String sampleOne, String sampleTwo){
+		byte temp[] = new byte[2];
+
+		char samplePosThree[] = new char[2];
+		char samplePosOne[] = new char[2];
+
+		samplePosThree[0] = sampleOne.charAt(0);
+		samplePosThree[1] = sampleTwo.charAt(0);
+
+		samplePosOne[0] = sampleOne.charAt(2);
+		samplePosOne[1] = sampleTwo.charAt(2);
+
+		//Check whether position three contains the id
+
+		boolean decision[] = new boolean[2];
+		int posThreeValue[] = new int[2];
+		int posOneValue[] = new int[2];
+
+		for(int i = 0; i < 2; i++){
+			posThreeValue[i] = Integer.parseInt(samplePosThree[i] + "",16);
+			posOneValue[i] = Integer.parseInt(samplePosOne[i] + "",16);
+
+		}
+		decision[0] = checkSequence(posThreeValue[0],posThreeValue[1]);
+		decision[1] = checkSequence(posOneValue[0],posOneValue[1]);
+
+		if(!(decision[0]|decision[1])){
+			System.out.println("DBG: SampleOne: " + sampleOne + " SampleTwo: " + sampleTwo);
+		}
+
+		if(decision[0]){
+			return 0;
+		}else if(decision[1]){
+			return 2;
+		}else return -1;
+
+	}
+
+	private static boolean checkSequence(int a, int b){
+
+		if(a == b){
+			return false;
+		}
+
+		if(a > b){
+			return true;
+		}else if(b > a){
+			return true;
+		}else return false;
+
+	}
+
+
+
+	public static void main ( String[] args){
+		new TivaSerialDataReader();
+	}
+}
